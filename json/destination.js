9be43902283235d79b677100057c6e7267ae5940
Paolo Ricca <p.ricca-cons@almaviva.it>
2016-04-05 13:22:19 +0200

    first commit

---
+++ json/destination.js
@@ -0,0 +1,221 @@
+﻿var destinations =
+    {
+        continents: [
+            {
+                descE: {
+                    ita: "Nord e Centro America",
+                    eng: "Lorem Ipsum"
+                },
+                destination: [
+                    {
+                        descE: {
+                            ita: "Stati Uniti",
+                            eng: "Lorem Ipsum"
+                        }
+                    },
+                    {
+                        descE: {
+                            ita: "Messico",
+                            eng: "Lorem Ipsum"
+                        }
+                    },
+                    {
+                        descE: {
+                            ita: "Caraibi",
+                            eng: "Lorem Ipsum"
+                        }
+                    }
+                ]
+            },
+            {
+                descE: {
+                    ita: "Oceano Pacifico",
+                    eng: "Lorem Ipsum"
+                },
+                destination: [
+                    {
+                        descE: {
+                            ita: "Polinesia",
+                            eng: "Lorem Ipsum"
+                        }
+                    },
+                    {
+                        descE: {
+                            ita: "Rapa Nui",
+                            eng: "Lorem Ipsum"
+                        }
+                    }
+                ]
+            },
+            {
+                descE: {
+                    ita: "Oceano Indiano",
+                    eng: "Lorem Ipsum"
+                },
+                destination: [
+                    {
+                        descE: {
+                            ita: "Mauritius",
+                            eng: "Lorem Ipsum"
+                        }
+                    },
+                    {
+                        descE: {
+                            ita: "Seychelles",
+                            eng: "Lorem Ipsum"
+                        }
+                    },
+                    {
+                        descE: {
+                            ita: "Reunion",
+                            eng: "Lorem Ipsum"
+                        }
+                    },
+                    {
+                        descE: {
+                            ita: "Maldive",
+                            eng: "Lorem Ipsum"
+                        }
+                    },
+                    {
+                        descE: {
+                            ita: "Sri Lanka",
+                            eng: "Lorem Ipsum"
+                        }
+                    }
+                ]
+            },
+            {
+                descE: {
+                    ita: "Medio Oriente",
+                    eng: "Lorem Ipsum"
+                },
+                destination: [
+                    {
+                        descE: {
+                            ita: "Dubai",
+                            eng: "Lorem Ipsum"
+                        }
+                    },
+                    {
+                        descE: {
+                            ita: "Abu Dhabi",
+                            eng: "Lorem Ipsum"
+                        }
+                    },
+                    {
+                        descE: {
+                            ita: "Ras al Khaimah",
+                            eng: "Lorem Ipsum"
+                        }
+                    },
+                    {
+                        descE: {
+                            ita: "Oman",
+                            eng: "Lorem Ipsum"
+                        }
+                    },
+                    {
+                        descE: {
+                            ita: "Giordania",
+                            eng: "Lorem Ipsum"
+                        }
+                    }
+                ]
+            },
+            {
+                descE: {
+                    ita: "Estremo Oriente",
+                    eng: "Lorem Ipsum"
+                },
+                destination: [
+                    {
+                        descE: {
+                            ita: "Thailandia",
+                            eng: "Lorem Ipsum"
+                        }
+                    },
+                    {
+                        descE: {
+                            ita: "Cambogia",
+                            eng: "Lorem Ipsum"
+                        }
+                    },
+                    {
+                        descE: {
+                            ita: "Vietnam",
+                            eng: "Lorem Ipsum"
+                        }
+                    },
+                    {
+                        descE: {
+                            ita: "Laos",
+                            eng: "Lorem Ipsum"
+                        }
+                    },
+                    {
+                        descE: {
+                            ita: "Indonesia",
+                            eng: "Lorem Ipsum"
+                        }
+                    },
+                    {
+                        descE: {
+                            ita: "Malesia",
+                            eng: "Lorem Ipsum"
+                        }
+                    },
+                    {
+                        descE: {
+                            ita: "Giappone",
+                            eng: "Lorem Ipsum"
+                        }
+                    },
+                    {
+                        descE: {
+                            ita: "Hong Kong",
+                            eng: "Lorem Ipsum"
+                        }
+                    },
+                    {
+                        descE: {
+                            ita: "Singapore",
+                            eng: "Lorem Ipsum"
+                        }
+                    }
+                ]
+            },
+            {
+                descE: {
+                    ita: "Africa Australe",
+                    eng: "Lorem Ipsum"
+                },
+                destination: [
+                    {
+                        descE: {
+                            ita: "Sudafrica",
+                            eng: "Lorem Ipsum"
+                        }
+                    },
+                    {
+                        descE: {
+                            ita: "Mozambico",
+                            eng: "Lorem Ipsum"
+                        }
+                    },
+                    {
+                        descE: {
+                            ita: "Botswana",
+                            eng: "Lorem Ipsum"
+                        }
+                    },
+                    {
+                        descE: {
+                            ita: "Namibia",
+                            eng: "Lorem Ipsum"
+                        }
+                    },
+                ]
+            },
+        ]
+    }
